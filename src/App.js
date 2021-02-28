464cd1847b93eeac6bdffe7011d1f10f68de88c3
Sjoerd Vink <sjoerdvink@icloud.com>
2021-02-28 13:47:19 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const tijd = "0.10.00";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>tinder</h1>
+    </div>
+  );
+}
+
+export default App;
